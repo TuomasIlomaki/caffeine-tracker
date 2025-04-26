abc87c8d1065ac301adc02d1bacddd52f8a84788
joyhillgames <144593357+joyhillgames@users.noreply.github.com>
2025-04-26 19:58:24 +0300

    Initial commit

---
+++ src/components/Layout.jsx
@@ -0,0 +1,36 @@
+export default function Layout(props) {
+    const { children } = props
+
+    const header = (
+        <header>
+            <div>
+                <h1 className="text-gradient">CAFFIEND</h1>
+                <p>For Coffee Insatiates</p>
+            </div>
+            <button>
+                <p>Sign up free</p>
+                <i className="fa-solid fa-mug-hot"></i>
+            </button>
+        </header>
+    )
+
+    const footer = (
+        <footer>
+            <p><span className="text-gradient">Caffiend</span> was made by 
+                <a href="https://github.com/TuomasIlomaki" target="_blank"> Thomas</a><br /> using the 
+                <a href="https://fantacss.smoljames.com" target="_blank"> FantaCSS design library.</a><br />Check out the project on
+                <a href="https://github.com/TuomasIlomaki" target="_blank"> GitHub</a>
+            </p>
+        </footer>
+    )
+
+    return (
+        <>
+            {header}
+            <main>
+                {children}
+            </main>
+            {footer}
+        </>
+    )
+}
